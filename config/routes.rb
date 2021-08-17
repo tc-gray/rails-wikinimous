d2b20ece231caaa7ee8f3941eb993038a98657b4
Tegan Cadney-Gray <tegan.cadney@gmail.com>
2021-08-17 18:49:29 +0100

    created an application to display articles using rails, no styling and issues with index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # get 'articles/index', to: 'articles#index'
+  # get 'articles/show', to: 'articles#show'
+  # get 'articles/new', to: 'articles#new'
+  # get 'articles/edit', to: 'articles#edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
 end
